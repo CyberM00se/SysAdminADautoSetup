5c79d8b925eb924d2bf5773caf664419e78eb230
Chromosom3 <dylannavarro6@gmail.com>
2020-12-08 12:01:53 -0500

    Automation stager upload.

---
+++ automation/ad-automation-stager.ps1
@@ -0,0 +1,10 @@
+# Script: ad-automation-stager.ps1
+# Author: Dylan 'Chromosome' Navarro
+# Description: Downloads all the necessary files for AD automation and configures tasks to complete the process. 
+
+# This will need to be changed from the development branch once testing is complete.
+$file2download = "https://raw.githubusercontent.com/CyPH3RSkULL5/SysAdminADautoSetup/dylan-development/install-adds.ps1"
+
+(New-Object System.Net.WebClient).DownloadFile($file2download,"automation.zip")
+
+Expand-Archive -Path automation.zip
