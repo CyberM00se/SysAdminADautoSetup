e14a251ca9e2e09de6833ab4e44a4701cfc219ac
Chromosom3 <dylannavarro6@gmail.com>
2020-12-08 13:40:29 -0500

    Worked on scheduletask function in stager.

--- ad-automation-stager.ps1
+++ ad-automation-stager.ps1
@@ -6,16 +6,13 @@
 $file2download = "https://raw.githubusercontent.com/CyPH3RSkULL5/SysAdminADautoSetup/dylan-development/zip.zip"
 $currentPath = (Get-Location).path
 
-(New-Object System.Net.WebClient).DownloadFile($file2download,"automation.zip")  # Downloads the ZIP archive from the repo. 
-Expand-Archive -Path automation.zip -DestinationPath .  # Uncompresses the archine in the current directory (.). 
+(New-Object System.Net.WebClient).DownloadFile($file2download,"zip.zip")  # Downloads the ZIP archive from the repo. 
+Expand-Archive -Path zip.zip -DestinationPath .  # Uncompresses the archine in the current directory (.). 
 Remove-Item -Path .\zip.zip  # Cleans up the current directory by removing the zip file. 
 
 function scheduletask {
-    $action = New-ScheduledTaskAction -Execute "$currentPath\automation\configure-ad.ps1"
-    $trigger = New-ScheduledTaskTrigger -Once -AtStartup
-    $principal = New-ScheduledTaskPrincipal "Administrator"
-    $task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger
-    Register-ScheduledTask -TaskName "AD Automation" -Description "https://github.com/CyPH3RSkULL5/SysAdminADautoSetup/tree/main" -InputObject $task
+    $trigger = New-JobTrigger -AtStartup -RandomDelay 00:00:30
+    Register-ScheduledJob -Trigger $trigger -FilePath "$currentPath\automation\configure-ad.ps1" -Name "AD Automation"  # Use Get-Job to see if job was complete.
 }
 
 scheduletask
