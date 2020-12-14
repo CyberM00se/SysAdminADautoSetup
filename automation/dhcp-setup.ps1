353b29a19a85f2ddc1ad89c65cb7661a638a680f
Noah Beckman <36571775+CyPH3RSkULL5@users.noreply.github.com>
2020-12-14 15:14:38 -0800

    Add files via upload

--- automation/dhcp-setup.ps1
+++ automation/dhcp-setup.ps1
@@ -5,6 +5,7 @@
 #Find the DHCP Server Name
 Get-WindowsFeature | Where-Object Name -like "*DHCP*"
 
+Write-Output "Installing DHCP with Managmenet Tools"
 #Intall it with management tools
 Install-WindowsFeature DHCP -IncludeManagementTools
 
@@ -12,30 +13,37 @@ Install-WindowsFeature DHCP -IncludeManagementTools
 
 #region create Security Groups
 
-#Current Groups
-($([ADSI]"WinNT://$env:COMPUTERNAME").Children | Where-Object SchemaClassName -eq 'group').name
+Write-Output "Creating Security Groups"
 
-#Use netsh to create new ones
+#Create a new security group
 netsh dhcp add securitygroups
 
-#verify group creation
-($([ADSI]"WinNT://$env:COMPUTERNAME").Children | Where-Object SchemaClassName -eq 'group').name
-
 #Restart the DHCP server service
+Write-Output "Restarting DHCP Service"
 Restart-Service dhcpserver
 
 #endregion
 
-#region Authorize
-Get-DhcpServerInDC
+#region Add Scope
 
-#Get IP for the dhcp server
-Get-NetIPAddress -InterfaceAlias Ethernet -AddressFamily IPv4
-$LocalFQDN = (Resolve-DnsName $env:COMPUTERNAME | Where-Object Type -eq 'A').Name
+#add input section here for name of scope
+#add input section here for start and end range
 
-#Authorize the server
-Add-DhcpServerInDC -DnsName $LocalFQDN -IPAddress $LocalIP
+Add-DhcpServerv4Scope -ComputerName $env:COMPUTERNAME -Name "testDhcpscope" -StartRange 10.0.1.10 -EndRange 10.0.2.100 -SubnetMask 255.255.255.0 -LeaseDuration 8:0:0:0
 
-#Verify
-Get-DhcpServerInDC
+#get the default gateway
+$myDefaultGateway = Get-NetIPConfiguration | foreach IPv4DefaultGateway
+
+#get the domain name
+$myDomainName = (Get-WmiObject Win32_ComputerSystem).Domain
+
+#get the local machines Ip address
+$localIP = (Get-NetIPAddress -InterfaceAlias Ethernet -AddressFamily IPv4).IPAddress
+
+#set the dhcp settings
+Set-DhcpServerv4Optionvalue -computername $env:COMPUTERNAME -Router $myDefaultGateway -dns 127.0.0.1 -DnsDomain $myDomainName
+
+#authorize the dhcp server for ad network
+Add-DhcpServerInDC $env:COMPUTERNAME $localIP
+
 #endregion
